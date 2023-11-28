062e44be042e56644963057de6a8a2877b8f949c
Yazan Alzubi <yalzubi@uolinc.com>
2023-11-28 11:09:38 -0300

    Adding the login screen validation

--- src/pages/LoginWindowsInterface/index.jsx
+++ src/pages/LoginWindowsInterface/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Grid,
   Head,
@@ -9,11 +9,60 @@ import {
   LeftSide,
   MainUser,
   GuestUser,
+  Loading,
+  Container,
+  ActiveMainUser,
+  InputContainer,
+  Error,
+  LoadingMainAccountContainer,
+  LoadingImageMainAccount,
 } from "./styles";
-import { duck, windowsXPLogo, turnoffwindows, chess } from "../../assets";
+import { duck, windowsXPLogo, turnoffwindows, chess, Go } from "../../assets";
 import { useNavigate } from "react-router-dom";
+
+import Tooltip from "@mui/material/Tooltip";
+import Fade from "@mui/material/Fade";
+
 function Login() {
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [showPasswordInterface, setShowPasswordInterface] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const [account, setAccount] = useState("");
+  console.log("account", account);
+  const handleLogin = (account) => {
+    if (account === "Main") {
+      setAccount("Main");
+    }
+
+    if (account === "Guest") {
+      setAccount("Guest");
+    }
+
+    if (inputValue === "evanescence2008") {
+      setIsLoading(true);
+      setTimeout(() => {
+        navigate("/desktop");
+      }, 3000);
+    } else {
+      setPasswordError(true);
+    }
+  };
+
+  useEffect(() => {
+    if (account === "Guest") {
+      setIsLoading(true);
+      setTimeout(() => {
+        navigate("/desktop");
+      }, 3000);
+    }
+  }, [account]);
+
+  const validatePassword = (value) => {
+    setInputValue(value);
+  };
+
   return (
     <Grid>
       <Head />
@@ -23,15 +72,92 @@ function Login() {
             <img src={windowsXPLogo} alt="windows XP Logo" />
             <div>To begin, click your user name</div>
           </LeftSide>
-          <RightSideContent>
-            <MainUser>
-              <img src={duck} /> <span>The Dark Knight</span>
-            </MainUser>
-            <GuestUser>
-              <img src={chess} onClick={() => navigate("/desktop")} />{" "}
-              <span>Guest</span>
-            </GuestUser>
-          </RightSideContent>
+
+          {!isLoading && (
+            <>
+              <RightSideContent>
+                <>
+                  {showPasswordInterface ? (
+                    <>
+                      <Container>
+                        <ActiveMainUser>
+                          <img src={duck} />
+                          <div>
+                            <span>The Dark Knight</span>
+                            <br />
+                            Type your password
+                          </div>
+                        </ActiveMainUser>
+                      </Container>
+                      <InputContainer>
+                        <input
+                          type="password"
+                          value={inputValue}
+                          onChange={(e) => validatePassword(e.target.value)}
+                        />
+                        {passwordError && (
+                          <Error>Dude..I gave you a hint</Error>
+                        )}
+                        <img src={Go} onClick={() => handleLogin("Main")} />
+                        <Tooltip
+                          TransitionComponent={Fade}
+                          TransitionProps={{ timeout: 1000 }}
+                          title="Password is: evanescence2008">
+                          <span>?</span>
+                        </Tooltip>
+                      </InputContainer>
+                    </>
+                  ) : (
+                    <>
+                      <MainUser onClick={() => setShowPasswordInterface(true)}>
+                        <img src={duck} /> <span>The Dark Knight</span>
+                      </MainUser>
+                    </>
+                  )}
+                </>
+
+                <GuestUser>
+                  <img src={chess} onClick={() => handleLogin("Guest")} />{" "}
+                  <span>
+                    <span>Guest</span>
+                    <br />
+                    {isLoading && (
+                      <Loading>loading your personal settings</Loading>
+                    )}
+                  </span>
+                </GuestUser>
+              </RightSideContent>
+            </>
+          )}
+
+          {isLoading && account === "Main" && (
+            <>
+              <LoadingMainAccountContainer>
+                <>
+                  <LoadingImageMainAccount src={duck} />
+                  <h1>The Dark Knight</h1>
+                  <Loading>loading your personal settings..</Loading>
+                </>
+              </LoadingMainAccountContainer>
+            </>
+          )}
+
+          {isLoading && account === "Guest" && (
+            <>
+              <RightSideContent>
+                <GuestUser>
+                  <img src={chess} onClick={() => handleLogin("Guest")} />{" "}
+                  <span>
+                    <span>Guest</span>
+                    <br />
+                    {isLoading && (
+                      <Loading>loading your personal settings</Loading>
+                    )}
+                  </span>
+                </GuestUser>
+              </RightSideContent>
+            </>
+          )}
         </Center>
       </Body>
       <Footer>
--- src/pages/LoginWindowsInterface/styles.js
+++ src/pages/LoginWindowsInterface/styles.js
@@ -98,6 +98,65 @@ export const RightSideContent = styled.div`
   gap: 1rem;
 `;
 
+export const LoadingMainAccountContainer = styled.div`
+  display: flex;
+  justify-content: baseline;
+  div {
+    position: relative;
+    left: -247px;
+    bottom: -70px;
+  }
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+
+  align-items: center;
+  border: none;
+  outline: none;
+  border-radius: 5px;
+  position: relative;
+  top: -37px;
+  left: 120px;
+  gap: 0.5rem;
+  input {
+    font-size: 2rem;
+    width: 9rem;
+    height: 2rem;
+    outline: none;
+    border: none;
+    border-radius: 0.5rem;
+  }
+  img {
+    width: 2rem;
+    border-radius: 5px;
+    border: 1px solid #fff;
+    box-shadow: 2px 2px 0px 0px #00489a,
+      inset -1px -2px 4px 0 rgba(0, 0, 0, 0.7);
+  }
+  span {
+    background-color: #0573f3;
+    font-size: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 900;
+    color: #fff;
+    cursor: pointer;
+    border: 1px solid #fff;
+    border-radius: 5px;
+    width: 32px;
+    height: 32px;
+    background-image: linear-gradient(
+      150deg,
+      rgba(255, 255, 255, 0.7) 0%,
+      rgba(70, 161, 252, 0) 50%
+    );
+    box-shadow: 2px 2px 0px 0px #00489a,
+      inset -1px -2px 4px 0 rgba(0, 0, 0, 0.7);
+  }
+`;
+
 export const MainUser = styled.div`
   display: flex;
 
@@ -125,6 +184,22 @@ export const MainUser = styled.div`
       cursor: pointer;
       opacity: 100;
     }
+  }
+`;
+
+export const ActiveMainUser = styled.div`
+  display: flex;
+
+  img {
+    width: 80px;
+    height: 80px;
+    margin-right: 20px;
+    border: 3px solid #fff;
+    border-radius: 8px;
+  }
+
+  span {
+    font-size: 1.8rem;
   }
 `;
 
@@ -172,4 +247,45 @@ export const LeftSide = styled.div`
   border-image-slice: 0 1 0 0;
   border-right-style: solid;
   padding: 3rem;
+`;
+
+export const Loading = styled.div`
+  font-size: 1rem;
+  color: #004da3;
+`;
+
+export const Container = styled.div`
+  cursor: pointer;
+  background: linear-gradient(90deg, #00489a 60%, rgba(255, 255, 255, 0) 100%);
+  background-clip: padding-box;
+  outline: none;
+  cursor: pointer;
+  display: flex;
+  min-width: 400px;
+  max-width: 100%;
+  padding: 12px;
+  border-radius: 8px;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+  border: 1px solid transparent;
+  border-right: 0;
+  position: relative;
+  background-clip: padding-box;
+  border: 1px solid transparent;
+`;
+
+export const Error = styled.div`
+  position: absolute;
+  top: 40px;
+  max-width: 11rem;
+  font-size: 0.7rem;
+`;
+
+export const LoadingImageMainAccount = styled.img`
+  outline: 3px solid #ffd700;
+  width: 80px;
+  height: 80px;
+  margin-right: 20px;
+  border: 3px solid #fff;
+  border-radius: 8px;
 `;
