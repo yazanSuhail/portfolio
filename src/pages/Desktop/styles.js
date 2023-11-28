e3fd37bc7b1ede84504b59cf301d17490525a5a6
Yazan Alzubi <yalzubi@uolinc.com>
2023-11-28 11:15:22 -0300

    1

--- src/pages/Desktop/styles.js
+++ src/pages/Desktop/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const DesktopContainer = styled.div`
-  background-image: url("src/assets/desktop-background.png");
+  background-image: url("https://i.imgur.com/Zk6TR5k.jpg");
   background-size: cover;
   background-position: center center;
   background-repeat: no-repeat;
